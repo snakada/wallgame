d5fd95f029821c530761b58bce459f8fa388384b
Shota Nakada <nakada@qurumi.jp>
2018-02-23 07:04:23 +0900

    フェーズ2: 各DOMをjsで生成するようにしました。

---
+++ js/app.js
@@ -0,0 +1,7 @@
+/**
+ * アプリDOMを生成して
+ * フィールドDOMとスコアDOMを組み込みます。
+ */
+const $app = createDivWithId('app');
+appendDomChildren($app, [$field, $score]);
+
---
+++ js/wall.js
@@ -0,0 +1,23 @@
+/**
+ * 壁(枠)DOMを生成します。
+ */
+const $wall = createDivWithClassName('wall');
+setDomStyle($wall, 'right: 100px;');
+
+/**
+ * 壁(上)DOMを生成します。
+ */
+const $wallTop = createDivWithClassName('wallTop');
+setDomStyle($wallTop, 'height: 150px;');
+
+/**
+ * 壁(下)DOMを生成します。
+ */
+const $wallBottom = createDivWithClassName('wallBottom');
+setDomStyle($wallBottom, 'height: 50px;');
+
+/**
+ * 壁(枠)DOMに壁(上)と壁(下)DOMを組み込みます。
+ */
+appendDomChildren($wall, [$wallTop, $wallBottom]);
+
