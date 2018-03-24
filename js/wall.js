6060a03776cd117970ac200f81aff5e80e92e9bd
Shota Nakada <nakada@qurumi.jp>
2018-03-25 06:39:37 +0900

    setDomStyleの仕様を変更しました。

--- js/wall.js
+++ js/wall.js
@@ -2,19 +2,19 @@
  * 壁(枠)DOMを生成します。
  */
 const $wall = createDivWithClassName('wall');
-setDomStyle($wall, 'right: 100px;');
+setDomStyle($wall, 'right', '100px');
 
 /**
  * 壁(上)DOMを生成します。
  */
 const $wallTop = createDivWithClassName('wallTop');
-setDomStyle($wallTop, 'height: 150px;');
+setDomStyle($wallTop, 'height', '150px');
 
 /**
  * 壁(下)DOMを生成します。
  */
 const $wallBottom = createDivWithClassName('wallBottom');
-setDomStyle($wallBottom, 'height: 50px;');
+setDomStyle($wallBottom, 'height', '50px');
 
 /**
  * 壁(枠)DOMに壁(上)と壁(下)DOMを組み込みます。
