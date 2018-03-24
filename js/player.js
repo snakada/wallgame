7252f06c7cd1060ebcde31f6460c7b52a8f5e247
Shota Nakada <nakada@qurumi.jp>
2018-03-25 08:27:40 +0900

    プレイヤーが壁に当たらないようにしました。

--- js/player.js
+++ js/player.js
@@ -11,6 +11,39 @@ setDomStyle($player, 'left', '40px');
 const getPlayerPositions = () => getDomPositions($player)
 
 /**
+ * プレイヤーDOMが壁DOMに重なるか調べます。
+ */
+const playerOverlapsWall = (top, left, wallPosition) => {
+  const wallLeft = wallPosition.left;
+  const top2 = top + 20;
+  const wallTop = wallPosition.top;
+  const wallTop2 = wallTop + wallPosition.height;
+  if(
+    left === wallLeft
+    && (
+         (top > wallTop && top < wallTop2)
+      || (top2 > wallTop && top2 < wallTop2)
+      || (top === wallTop && top2 === wallTop2)
+    )
+  ) {
+    return true;
+  }
+  return false;
+};
+
+/**
+ * プレイヤーDOMを動かしていい場所か調べます。
+ */
+const allowMovePlayer = (top, bottom, left, right) => {
+  if(top < 0 || bottom < 0 || left < 0 || right < 0) {
+    return false;
+  }
+  return getDomPositionsAllWalls().every(wallPosition => {
+    return !playerOverlapsWall(top, left, wallPosition);
+  });
+};
+
+/**
  * プレイヤーDOMを動かします。
  */
 const movePlayer = (moveTop, moveLeft) => {
@@ -20,8 +53,10 @@ const movePlayer = (moveTop, moveLeft) => {
   const newLeft = left + moveLeft;
   const newRight = right - moveLeft;
 
-  setDomStyle($player, 'top', `${newTop}px`);
-  setDomStyle($player, 'left', `${newLeft}px`);
+  if(allowMovePlayer(newTop, newBottom, newLeft, newRight)) {
+    setDomStyle($player, 'top', `${newTop}px`);
+    setDomStyle($player, 'left', `${newLeft}px`);
+  }
 };
 
 const movePlayerUp = () => movePlayer(-20, 0);
--- js/wall.js
+++ js/wall.js
@@ -8,7 +8,7 @@ setDomStyle($wall, 'right', '100px');
  * 壁(上)DOMを生成します。
  */
 const $wallTop = createDivWithClassName('wallTop');
-setDomStyle($wallTop, 'height', '150px');
+setDomStyle($wallTop, 'height', '140px');
 
 /**
  * 壁(下)DOMを生成します。
@@ -20,4 +20,18 @@ setDomStyle($wallBottom, 'height', '50px');
  * 壁(枠)DOMに壁(上)と壁(下)DOMを組み込みます。
  */
 appendDomChildren($wall, [$wallTop, $wallBottom]);
+
+/**
+ * すべての壁の位置情報を返します。
+ */
+const getDomPositionsAllWalls = () => {
+  const $wallParts = findDomAll('.wallTop, .wallBottom');
+  return [...$wallParts].map($wallPart => {
+    const parentPosition = getDomPositions($wallPart.parentNode);
+    const returnPosition = getDomPositions($wallPart);
+    returnPosition.left = parentPosition.left;
+    returnPosition.right = parentPosition.right;
+    return returnPosition;
+  });
+};
 
