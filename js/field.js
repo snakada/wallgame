949e5764f658af19cd82c1ffbd955f745d213bcb
Shota Nakada <nakada@qurumi.jp>
2018-03-25 08:41:16 +0900

    壁をランダムに生成するようにしました。

--- js/field.js
+++ js/field.js
@@ -3,5 +3,12 @@
  * プレイヤーDOMと壁(枠)DOMを組み込みます。
  */
 const $field = createDivWithId('field');
-appendDomChildren($field, [$player, $wall]);
+appendDomChild($field, $player);
+
+/**
+ * フィールドDOMに壁DOMを組み込んでみます。
+ */
+appendDomChild($field, createWall(280));
+appendDomChild($field, createWall(140));
+appendDomChild($field, createWall(0));
 
