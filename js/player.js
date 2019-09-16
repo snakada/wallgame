cec1944745acefca3ee11951ec9b84d0789c2726
Shota Nakada <nakada@qurumi.jp>
2019-09-17 02:44:11 +0900

    player.js の不要な変数を削除等しました。

--- js/player.js
+++ js/player.js
@@ -7,26 +7,31 @@ setDomStyle($player, 'left', '40px');
 
 /**
  * 現在のプレイヤーの位置を取得します。
+ * @return {object} pos        プレイヤーのポジション関連のCSSプロパティ値のオブジェクト
+ * @return {number} pos.top
+ * @return {number} pos.bottom
+ * @return {number} pos.left
+ * @return {number} pos.right
+ * @return {number} pos.height
  */
 const getPlayerPositions = () => getDomPositions($player)
 
 /**
  * プレイヤーDOMを動かします。
+ * @param {number} moveTop  垂直方向に動かす距離(px)
+ * @param {number} moveLeft 水平方向に動かす距離(px)
  */
 const movePlayer = (moveTop, moveLeft) => {
-  const {top, bottom, left, right} = getPlayerPositions();
-  const newTop = top + moveTop;
-  const newBottom = bottom - moveTop;
+  const {top, left} = getPlayerPositions();
+  const newTop  = top  + moveTop;
   const newLeft = left + moveLeft;
-  const newRight = right - moveLeft;
-
   setDomStyle($player, 'top', `${newTop}px`);
   setDomStyle($player, 'left', `${newLeft}px`);
 };
 
 /**
- * プレイヤーDOMを上下左右へ1マス分(20px)移動させる
- * 関数をそれぞれ定義しておきます。
+ * プレイヤーDOMを上下左右へ1マス分(20px)移動させる関数を
+ * それぞれ定義しておきます。
  */
 const movePlayerUp    = () => movePlayer(-20, 0);
 const movePlayerDown  = () => movePlayer(20, 0);
